e907668efa8f4de6aab781319339d28794027cdd
matthewsww <swwmatthew@gmail.com>
2022-12-15 15:25:38 +0800

    updating versions

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.2.8"
+  required_version = ">= 1.3.6"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.34.0"
+      version = "~> 4.46.0"
     }
   }
 }
