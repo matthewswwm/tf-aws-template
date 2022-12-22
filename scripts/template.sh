fbd1f2cc2a0451d24816b759a46eeb935af12de1
matthewsww <swwmatthew@gmail.com>
2022-12-22 17:18:35 +0800

    updated template

--- scripts/template.sh
+++ scripts/template.sh
@@ -9,4 +9,16 @@ set -x
 
 echo "[INFO]: Starting script"
 
+### Check input variables. Exit if wrong number is supplied
+if [[ $# -ne 2 ]]; then
+    echo "[ERROR]: No arguments supplied"
+    echo "[ERROR]: Usage: $0 USER_INPUT_1 USER_INPUT_2"
+    exit 1
+else
+   USER_INPUT_1="$1"
+   USER_INPUT_2="$2"
+fi
+
+echo $USER_INPUT_1 & $USER_INPUT_2
+
 echo "[INFO]: Script completed"
